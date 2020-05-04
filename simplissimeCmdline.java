e33887c92bf59f576f1868c16943b5d65f158ec4
BAUCHER Frederic <frederic.baucher@cegedim.fr>
2020-05-04 22:27:53 +0200

    traitement de la commande par defaur en cas d absence d arguments

--- simplissimeCmdline.java
+++ simplissimeCmdline.java
@@ -1,5 +1,6 @@
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.ParseException;
@@ -17,9 +18,10 @@ public class simplissimeCmdline {
             opt.addOption("h", false, "Aide de cette application en mode ligne de commande");
             opt.addOption("c", false, "[C]rud : ajouter une ville avec le nom et le code postal (nom, code postal)");
             opt.addOption("r", false, "c[R]ud : retrouver une ville en fonction d'un parametre (id, nom, code postal)");
-            opt.addOption("u", false, "cr[U]d : mettre a jour une ville avec l'id, le nom et le code postal > simplissimeLocal -u -id 21 -nom MARRAKECH -cp 40160");
+            opt.addOption("u", false, "cr[U]d : mettre a jour une ville avec l'id, le nom et le code postal > simplissimeCmdline -u -id 21 -nom MARRAKECH -cp 40160");
             opt.addOption("d", false, "cru[D] : effacer une ville en fonction du parametre fourni (id, nom, code postal)");
             opt.addOption("l", false, "lister tous les enregistrements de la table villes");
+            opt.addOption("e", false, "ecraser la table villes");
             opt.addOption("nom", true, "nom de la ville" );
             opt.addOption("cp", true, "code postal de la ville" );
             opt.addOption("id", true, "identifiant de la ville" );
@@ -28,7 +30,7 @@ public class simplissimeCmdline {
             CommandLine cl = parser.parse(opt, args);
 
 			// afficher une aide qui documente chaque option
-			// java -cp ".\postgresql-42.2.5.jar;commons-cli-1.4.jar;." simplissimeLocal -h
+			// java -cp ".\postgresql-42.2.5.jar;commons-cli-1.4.jar;." simplissimeCmdline -h
             if ( cl.hasOption('h') ) {
                 HelpFormatter f = new HelpFormatter();
                 f.printHelp("Aide de simplissime", opt);
@@ -37,7 +39,7 @@ public class simplissimeCmdline {
 			String display = "" ;
 			txn = txnscript.getTxnscript() ;
 			
-			// java -cp ".\postgresql-42.2.5.jar;commons-cli-1.4.jar;." simplissimeLocal -u -id 178 -nom MER -cp 41500
+			// java -cp ".\postgresql-42.2.5.jar;commons-cli-1.4.jar;." simplissimeCmdline -u -id 178 -nom MER -cp 41500
             if ( cl.hasOption('u') )
 			{
 					System.out.println( "\n\nMettre a jour encore le code postal dans la base\n" ) ;
@@ -50,14 +52,30 @@ public class simplissimeCmdline {
 					System.out.println( display ) ;
             }
 
-			// java -cp ".\postgresql-42.2.5.jar;commons-cli-1.4.jar;." simplissimeLocal -l
+			// java -cp ".\postgresql-42.2.5.jar;commons-cli-1.4.jar;." simplissimeCmdline -e
+            if ( cl.hasOption('e') )
+			{
+					System.out.println( "\n\nEcraser table villes \n" ) ;
+					
+					String jdbcUrl = txnscript.getJdbcUrl () ;
+					jdbcUnitUtilities jdbcUtilities = jdbcUnitUtilities.getJdbcUnitUtilities( jdbcUrl ) ;
+					boolean testDelete = jdbcUtilities.deleteTableContent ( "villes" ) ; 
+					// display = txn.list () ;
+					// System.out.println( display ) ;
+            }
+			
+			// java -cp ".\postgresql-42.2.5.jar;commons-cli-1.4.jar;." simplissimeCmdline -l
             if ( cl.hasOption('l') )
 			{
 					System.out.println( "\n\nEtat initial de la base\n" ) ;
 					display = txn.list () ;
 					System.out.println( display ) ;
             }
-            else
+			
+			
+			// dans le cas ou il n y a aucune option, argument sur la ligne de commande
+			Option[] opts = cl.getOptions();			
+            if (opts == null) 
 			{
 				// pot-pourri de méthodes du txnScript
 				boolean isOk = txn.check() ;
